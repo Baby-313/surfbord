3e38464684b4c09bd3573b744f6823ab8bb9e9c3
Юсуф Джанибеков <yusufdjanibekov@MacBook-Air-Usuf.local>
2023-02-05 13:42:50 +0300

    update js

--- js/modal.js
+++ js/modal.js
@@ -25,3 +25,44 @@ function createModal(content) {
   })
   return overlayElement;
 }
+const leftbtn=document.querySelector("#left");
+const rightbtn=document.querySelector("#right");
+const items=document.querySelector("#list");
+const computedStyle=getComputedStyle(list);
+var offsetWidth = document.getElementById('slide').offsetWidth;
+var offsetHeight = document.getElementById('slide').offsetHeight;
+alert(`${offsetWidth} x ${offsetHeight}`);
+
+let currentRight=0;
+
+rightbtn.addEventListener("click",e=>{
+    e.preventDefault();
+    let currentRight=parseInt(computedStyle.right)
+
+    if (currentRight<100) {
+        items.style.right=`${currentRight+offsetWidth}px`;      
+    }
+});
+
+leftbtn.addEventListener("click",e=>{
+    e.preventDefault();
+    let currentRight=parseInt(computedStyle.right)
+    
+    if (currentRight>0) {
+        items.style.right=`${currentRight-offsetWidth}px`;      
+    }
+});
+
+$(".click__info").click(function (e) {
+	e.preventDefault();
+	
+	var menu = $(this).closest('.humans__list');
+	
+	if (false == $(this).next().is(':visible')) {
+		menu.find('.human__info').removeClass('slide active');
+		menu.find('.human__info').slideUp();
+	}
+	
+	$(this).next().slideToggle();
+	$(this).parent().addClass('slide');
+});
